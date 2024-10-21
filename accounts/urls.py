5b591e16efac4b53f0275e52e5e4558298d24307
kia <kiarashoseinzadeh@gmail.com>
2024-10-21 16:42:15 +0330

    login edit

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('sing_up/', views.sing_up_views, name='sing_up'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render ,redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login , logout
 
 def sing_up_views (request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'account created successfully')
+            user = form.save()
+            login(request, user)
             #login
+
             return redirect ('articles:List')
     else:
         form = UserCreationForm()
-        return render(request,'accounts/sing_up.html',{'form':form})
+    return render(request,'accounts/sing_up.html',{'form':form})
+
+def login_view (request):
+    if request.method == "POST":
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #login user
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:List')
+    else:
+        form=AuthenticationForm()
+    return render(request,"accounts/login.html",{'form':form})
+
+
+def logout_view (request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('articles:List')
